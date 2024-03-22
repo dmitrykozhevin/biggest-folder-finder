7c2b8869bd15efd65de7c357e4ba97fdc312645a
dmitrii_kozhevin <d.kojevin@mail.ru>
2024-03-22 13:35:58 +0200

    find size of all files and folder in chosen folder

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+
+
 public class Main {
     public static void main(String[] args) {
+        String folderPath = "D:/Films";
+        File file = new File(folderPath);
 
+
+        System.out.println(getFolderSize(file));
+
+//        Показать директорию проекта:
+//        System.out.println(System.getProperties().get("user.dir"));
+    }
+    public static long getFolderSize (File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
